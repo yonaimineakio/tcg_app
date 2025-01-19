4b0f10bf6821911a1bb817d9e316219f493026d3
yonaimineakio <43339576+yonaimineakio@users.noreply.github.com>
2025-01-19 19:17:16 +0900

    fix Mypage Route

--- app/admin/mypage/[id]/edit/page.tsx
+++
@@ -1,8 +0,0 @@
-export default async function Page(props: {params : Promise<{id: string}>}) {
-  const params = await props.params
-  const id = params.id
-
-  return (
-    <h1>This is MyPage edit Page{id}</h1>
-  )
-}
---
+++ app/admin/mypage/edit/page.tsx
@@ -0,0 +1,6 @@
+export default async function Page() {
+
+  return (
+    <h1>This is MyPage edit Page</h1>
+  )
+}
