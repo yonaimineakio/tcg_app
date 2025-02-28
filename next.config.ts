111c5d9823573b757b0fb460f6dc1da80fbf16cb
yonaimineakio <43339576+yonaimineakio@users.noreply.github.com>
2025-02-28 21:47:24 +0900

    fix to deploy code to production properly

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,14 @@
+
+/** @type {import('next').NextConfig} */
+
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
   env: {
     DATABASE_URL: process.env.DATABASE_URL,
-  },
+  }
+
 };
 
 export default nextConfig;
