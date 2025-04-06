ac7c6ad4073bb5dcbcd014e85f86de423d18b60a
yonaimineakio <43339576+yonaimineakio@users.noreply.github.com>
2025-04-06 13:16:07 +0900

    fixced to pass vercel deploy

--- app/user/stores/[id]/page.tsx
+++ app/user/stores/[id]/page.tsx
@@ -1,20 +1,9 @@
 // app/user/stores/[id]/page.tsx
 import StoreMypage from '@/components/store-mypage';
-import { Suspense } from 'react';
-import Loading from '@/app/loading';
-// 正しいページパラメータの型定義
-interface PageParams {
-  id: string;
-}
 
-// サーバーコンポーネントとして実装
-export default async function Page({
-  params,
-}: {
-  params: PageParams;
-}) {
-  // クライアントコンポーネントを通常通り使用可能
-  return <Suspense fallback={<Loading />}>
-    <StoreMypage id={params.id} />
-  </Suspense>;
+
+export default async function Page(props: { params: Promise<{ id: string }> }) {
+  
+  const params = await props.params;
+  return <StoreMypage id={params.id} />
 }
