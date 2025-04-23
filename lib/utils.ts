4cbebff60f92cb6d612cb8fa46da4e669db575fd
yonaimineakio <43339576+yonaimineakio@users.noreply.github.com>
2025-04-23 20:41:00 +0900

    fixed to pass linter

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,3 @@
-// import { RRuleData } from "@/lib/definitions";
-import { CalendarEvent } from "@/lib/definitions";
-import { v4 as uuidv4 } from 'uuid';
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
   
